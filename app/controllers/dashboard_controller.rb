8ab218bf402cdfe92e9645fcd7d71152dcb9fc28
Kevin Arifin <kevarifin14@gmail.com>
2015-10-10 03:09:37 -0700

    allow user to access authentication from moves

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,17 @@ require 'oauth2'
 
 class DashboardController < ApplicationController
   def index
-    @client = OAuth2::Client.new(
+    @client = client
+  end
+
+  private
+
+  def client
+    OAuth2::Client.new(
       'nItFH0bEpvjZ941CM6JQqRrXmC69hpPi',
       'rll2y6Fz8CAInSV4JL5PDRo1FsKaZF5UQ6j_NG2pSASn86YtPj5Dr1HtSQ_9Mu8x',
-      site: 'https://api.moves-app.com',
+      site: 'https://api.moves-app.com/',
+      authorize_url: '/oauth/v1/authorize'
     )
-    @client.auth_code.authorize_url(redirect_uri: 'http://localhost:3000/')
   end
 end
