c323edf6dfbdcc438b70331f336f60493ec8875d
Taylor Wong <taylorkmw@gmail.com>
2015-10-10 19:03:30 -0700

    add #level to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def level
+  # Pokemon 'medium slow' experience formula
+    n = (1.0/6) * 5**(1.0/3) * (2 * (81 * self.experience_points**2-16695 * self.experience_points+1387600)**(1.0/2) + 18 * self.experience_points-1855)**(1.0/3)-(25 * 5**(2.0/3))/(2 * (2 * (81 * self.experience_points**2 - 16695 * self.experience_points+1387600)**(1.0/2)+18 * self.experience_points-1855)**(1.0/3))+25.0/6
+    n.round(0)
+  end
 end
