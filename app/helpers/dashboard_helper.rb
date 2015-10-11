7945dd67bf832300915978ab398abc5a6e959b98
Taylor Wong <taylorkmw@gmail.com>
2015-10-11 00:00:04 -0700

    fix level progress bar

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,22 +1,22 @@
 module DashboardHelper
   PROGRESS_BAR_WIDTH = 400 # px
 
-  def self.exp_req(lvl)
-    (6/5) * lvl**3 - 15 * lvl**2 + 100 * lvl - 140
+  def exp_req(lvl)
+    (6.0/5) * lvl**3 - 15 * lvl**2 + 100 * lvl - 140
   end
 
-  def self.level_upper_bound
+  def level_upper_bound
     lvl = current_user.level
     exp_req(lvl + 1)
   end
 
-  def self.level_lower_bound
+  def level_lower_bound
     lvl = current_user.level
     exp_req(lvl)
   end
 
-  def self.fill_width
-    PROGRESS_BAR_WIDTH * (current_user.experience_points - level_lower_bound) / (level_upper_bound - level_lower_bound)
+  def fill_width
+    (PROGRESS_BAR_WIDTH * (current_user.experience_points - level_lower_bound).to_f / (level_upper_bound - level_lower_bound)).to_i
   end
 
 end
