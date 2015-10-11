0764226816ef83b85f8134d00f446db7e6eb2750
Taylor Wong <taylorkmw@gmail.com>
2015-10-11 01:41:50 -0700

    add progress bar info

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -15,8 +15,12 @@ module DashboardHelper
     exp_req(lvl)
   end
 
+  def exp_percent
+    ((current_user.experience_points - level_lower_bound).to_f / (level_upper_bound - level_lower_bound)).round(2)
+  end
+
   def fill_width
-    (PROGRESS_BAR_WIDTH * (current_user.experience_points - level_lower_bound).to_f / (level_upper_bound - level_lower_bound)).to_i
+    (PROGRESS_BAR_WIDTH * exp_percent).to_i
   end
 
 end
